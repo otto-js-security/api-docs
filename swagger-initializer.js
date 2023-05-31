324894580123355b422f6d97e7a8343d4bf48b09
Josh <ascetik@gmail.com>
2023-05-31 15:22:52 -0500

    Updated Documentation

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./swagger-new.yaml",
+    url: "https://otto-js-security.github.io/api-docs/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
